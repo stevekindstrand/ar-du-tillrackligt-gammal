5f0a91736604f6b76b6932aa5f7f6197a9702b5a
stevekindstrand <stevekindstrand@gmail.com>
2021-11-23 13:58:17 +0100

    second commit (la till min value, 0-17 år

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.body.append(pElement)
 
 let btn = document.getElementById("btn")
 btn.addEventListener("click", function() {
-    if (age.value < 18) {
+    if (age.value > 0 && age.value < 18) {
         pElement.innerText = "Du är för ung!"
     } else if (age.value >= 18 && age.value < 65) {
         pElement.innerText = "Du är tillräckligt gammal!"
