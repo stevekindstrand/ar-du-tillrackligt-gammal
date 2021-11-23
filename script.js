43db3b2a0204e49fb1c01c2aa8259e08a713d6a9
stevekindstrand <stevekindstrand@gmail.com>
2021-11-23 13:54:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let age = document.getElementById("age")
+
+let pElement = document.createElement("p")
+document.body.append(pElement)
+
+let btn = document.getElementById("btn")
+btn.addEventListener("click", function() {
+    if (age.value < 18) {
+        pElement.innerText = "Du är för ung!"
+    } else if (age.value >= 18 && age.value < 65) {
+        pElement.innerText = "Du är tillräckligt gammal!"
+    } else if (age.value >= 65) {
+        pElement.innerText = "Du är för gammal, gå och lägg dig!"
+    }
+})
